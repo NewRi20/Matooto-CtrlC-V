6841fa92c8778c53c2b1ace61fc367f44a77ca47
Wen <frondairwen@gmail.com>
2026-04-23 23:54:15 +0800

    Initial commit: Matooto App with Expo and Firebase

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // Update this path to match your Expo Router folders
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
